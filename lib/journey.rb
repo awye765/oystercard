4a95d36fdbe92b856010ca3df2c150a4a2a77a53
awye765 <awye765@googlemail.com>
2016-06-02 22:35:50 +0100

    Adding fare method to journey class plus tests

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,20 +2,51 @@ require_relative 'oystercard'
 
 class Journey
 
-  # PENALTY_FARE = 6
+  PENALTY_FARE = 6
+  MIN_FARE = 1
 
-  attr_reader :entry_station, :exit_station, :current_journey
+
+  attr_reader :current_journey, :complete, :journey_log, :entry_station, :exit_station
 
   def initialize(entry_station=nil)
     @current_journey = {}
-    @current_journey[:entry_station] = entry_station
+    @journey_log = []
+    current_journey_start(entry_station)
     @complete = false
   end
 
-  def finish(station)
-    @exit_station = station
+  def finish(exit_station=nil)
+    current_journey_end(exit_station)
+    journey_log_update
+    # current_journey_reset
     @complete = true
-    @entry_station = nil
+  end
+
+  def current_journey_start(entry_station)
+    @entry_station = entry_station
+    @current_journey[:entry_station] = entry_station
+  end
+
+  def current_journey_end(exit_station)
+    @exit_station = exit_station
+    @current_journey[:exit_station] = exit_station
+  end
+
+  def journey_log_update
+    @journey_log << current_journey
+  end
+
+  def current_journey_reset
+    @current_journey = {}
+  end
+
+  def fare
+    return PENALTY_FARE if penalty?
+    MIN_FARE
+  end
+
+  def penalty?
+    (!entry_station || !exit_station)
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,55 +1,99 @@
 require 'journey'
 
 describe Journey do
-  subject(:journey) {described_class.new()}
+  subject(:journey)  {Journey.new()}
   let(:entry_station) {double :station, zone: 1}
   let(:exit_station) {double :station, zone: 1}
 
   describe "#initialize" do
 
+    subject(:journey) {Journey.new()}
+
+    context "irrespective of whether entry_station provided" do
+
+      it "journey is set to NOT complete" do
+        expect(journey.complete).to eq false
+      end
+
+    end
+
     context "when there is NO entry_station" do
 
       it "entry_station is nil" do
-        expect(journey.current_journey[entry_station]).to eq nil
+        expect(journey.current_journey[:entry_station]).to eq nil
       end
 
     end
 
     context "when there IS an entry_station" do
 
+      subject(:journey) {Journey.new(entry_station)}
+
       it "entry_station is saved to current_journey hash" do
-        journey = Journey.new(entry_station)
-        expect(journey.current_journey[entry_station]).to eq entry_station
+        expect(journey.current_journey[:entry_station]).to eq entry_station
       end
 
     end
-
 
   end
 
   describe "#finish" do
 
-    subject(:journey) { described_class.new }
+    subject(:journey) { Journey.new }
 
-    it "saves finish station to current journey hash" do
+    # it "saves finish station to current journey hash" do
+    #   journey.finish(exit_station)
+    #   expect(journey.current_journey[:exit_station]).to eq exit_station
+    # end
 
+    it "saves current journey to journey_log array" do
+      journey.finish(exit_station)
+      expect(journey.journey_log).not_to be_empty
     end
 
-    it "saves journey to journey_log" do
+    # it "resets current_journey" do
+    #   journey.finish(exit_station)
+    #   expect(journey.current_journey).to be_empty
+    # end
 
-    end
-
-    it "resets current_journey" do
-
+    it "journey is set to BE complete" do
+      journey.finish(exit_station)
+      expect(journey.complete).to eq true
     end
 
   end
 
   describe "#fare" do
 
-  end
+    context "when touch in but no touch out" do
+      subject(:journey)  {Journey.new(entry_station)}
 
-  describe "#complete" do
+      it "charges penalty fare" do
+        journey.finish()
+        expect(journey.fare).to eq Journey::PENALTY_FARE
+      end
+
+    end
+
+    context "when no touch in but touch out" do
+      subject(:journey)  {Journey.new(entry_station)}
+
+      it "charges penalty fare" do
+        journey.finish(exit_station)
+        expect(journey.fare).to eq Journey::PENALTY_FARE
+      end
+
+    end
+
+    context "when touch in and touch out" do
+      subject(:journey)  {Journey.new(entry_station)}
+
+      it "charges minimum fare" do
+        journey.finish(exit_station)
+        expect(journey.fare).to eq Journey::MIN_FARE
+      end
+
+    end
 
   end
 
